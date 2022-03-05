888f78e2705e8710643d3276bac83b23bc0b1233
shadowLQ <415804410@qq.com>
2022-03-05 17:46:43 +0800

    feat:配置文件和pom修改

--- src/main/java/com/lq/lidar/LiDarApplication.java
+++ src/main/java/com/lq/lidar/LiDarApplication.java
@@ -1,9 +1,11 @@
 package com.lq.lidar;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lq.lidar.mapper")
 public class LiDarApplication {
 
     public static void main(String[] args) {
