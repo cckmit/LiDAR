12e6a9398e0b101708a468af82525b6604933251
shadowLQ <415804410@qq.com>
2022-04-02 22:38:05 +0800

    feat:label和value转换

--- src/main/java/com/lq/lidar/controller/CbaySysOfficeController.java
+++ src/main/java/com/lq/lidar/controller/CbaySysOfficeController.java
@@ -2,13 +2,11 @@ package com.lq.lidar.controller;
 
 
 import com.lq.lidar.common.core.controller.BaseController;
+import com.lq.lidar.common.core.domain.ResponseEntity;
 import com.lq.lidar.common.utils.DataConvert;
 import com.lq.lidar.entity.CbaySysOffice;
 import com.lq.lidar.service.ICbaySysOfficeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -22,16 +20,17 @@ import java.util.List;
  * @since 2022-03-16
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/system/cbaySysOffice")
 public class CbaySysOfficeController extends BaseController {
     @Resource
     ICbaySysOfficeService cbaySysOfficeService;
 
     @GetMapping("/getByOfcTypeCd/{ofcTypeCd}")
-    public List getByOfcTypeCd(@PathVariable String ofcTypeCd) {
+    public ResponseEntity getByOfcTypeCd(@PathVariable String ofcTypeCd) {
         List<CbaySysOffice> cbaySysOffices = cbaySysOfficeService.lambdaQuery().eq(CbaySysOffice::getOfcTypeCd, ofcTypeCd).list();
         List list = DataConvert.ListToLV(cbaySysOffices,CbaySysOffice::getOfcNm,CbaySysOffice::getOfcId);
-        return list;
+        return ResponseEntity.success(list);
     }
 }
 
