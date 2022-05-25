85609536cbbb33e0d43edc9c79afccc79b9879b3
shadowLQ <415804410@qq.com>
2022-05-25 20:49:02 +0800

    feat:增加耗时注解

--- src/main/java/com/lq/lidar/common/aspectj/TaskTimeAspect.java
+++ src/main/java/com/lq/lidar/common/aspectj/TaskTimeAspect.java
@@ -16,11 +16,12 @@ import org.springframework.util.StopWatch;
 public class TaskTimeAspect {
     private static final Logger log = LoggerFactory.getLogger(TaskTimeAspect.class);
 
-    StopWatch stopWatch = new StopWatch();
+    StopWatch stopWatch ;
 
     @Before("@annotation(controllerTaskTime)")
     public void doBefore(JoinPoint joinPoint, TaskTime controllerTaskTime) throws Throwable {
 //        startTime.set(System.currentTimeMillis());
+        stopWatch= new StopWatch();
         stopWatch.start();
         //接收到请求，记录请求内容
 //        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
@@ -44,6 +45,7 @@ public class TaskTimeAspect {
         String methodName = joinPoint.getSignature().getName();
         StringBuilder message = new StringBuilder();
         message.append(stopWatch.getTotalTimeMillis() / 1000.0);
+//        message.append("getTotalTimeMillis"+stopWatch.getTotalTimeMillis() / 1000.0);
         message.append(" seconds");
         log.info("请求URL:{}", ServletUtils.getRequest().getRequestURI());
         log.info("请求METHOD:{}", ServletUtils.getRequest().getMethod());
