b339d1630b35ffffac58739c62689f6585f203ab
shadowLQ <415804410@qq.com>
2022-06-21 23:01:00 +0800

    refactor:表结构

---
+++ src/main/java/com/lq/lidar/mapper/CbayBpBizPtnrBaseMapper.java
@@ -0,0 +1,16 @@
+package com.lq.lidar.mapper;
+
+import com.lq.lidar.domain.entity.CbayBpBizPtnrBase;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author LQ
+ * @since 2022-06-21
+ */
+public interface CbayBpBizPtnrBaseMapper extends BaseMapper<CbayBpBizPtnrBase> {
+
+}
