265f078f125e2861ce8fbdc2d56b2631b8f799c0
shadowLQ <415804410@qq.com>
2022-07-04 18:03:07 +0800

    feat:合作伙伴增加详情接口和删除接口

--- src/main/java/com/lq/lidar/controller/CbayBpBizPtnrAcctInfoController.java
+++ src/main/java/com/lq/lidar/controller/CbayBpBizPtnrAcctInfoController.java
@@ -6,10 +6,7 @@ import com.lq.lidar.common.core.domain.ResponseEntity;
 import com.lq.lidar.domain.entity.CbayBpBizPtnrAcctInfo;
 import com.lq.lidar.service.ICbayBpBizPtnrAcctInfoService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -17,7 +14,7 @@ import java.util.Map;
 
 /**
  * <p>
- *  合作伙伴账户前端控制器
+ * 合作伙伴账户前端控制器
  * </p>
  *
  * @author LQ
@@ -25,7 +22,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/system/cbayBpBizPtnrAcctInfo")
-public class CbayBpBizPtnrAcctInfoController  extends BaseController {
+public class CbayBpBizPtnrAcctInfoController extends BaseController {
     @Resource
     ICbayBpBizPtnrAcctInfoService cbayBpBizPtnrAcctInfoService;
 
@@ -40,12 +37,31 @@ public class CbayBpBizPtnrAcctInfoController  extends BaseController {
                 .list();
         return ResponseEntity.success(getDataTable(list));
     }
+
+    /**
+     * 合作伙伴下拉框
+     *
+     * @return
+     */
     @GetMapping("/findBpBizPtnrAcctInfo")
-    public ResponseEntity findBpBizPtnrAcctInfo(){
+    public ResponseEntity findBpBizPtnrAcctInfo() {
         List<Map<String, String>> bpBizPtnrAcctInfo = cbayBpBizPtnrAcctInfoService.findBpBizPtnrAcctInfo();
         return ResponseEntity.success(bpBizPtnrAcctInfo);
     }
 
+    @GetMapping("/findBpBizPtnrAcctInfoByBpBankAcctId")
+    public ResponseEntity findBpBizPtnrAcctInfoByBpBankAcctId(String bpBankAcctId) {
+        CbayBpBizPtnrAcctInfo cbayBpBizPtnrAcctInfo = cbayBpBizPtnrAcctInfoService.getById(bpBankAcctId);
+        return ResponseEntity.success(cbayBpBizPtnrAcctInfo);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity delete(String bpBankAcctId) {
+        if (cbayBpBizPtnrAcctInfoService.removeById(bpBankAcctId)) {
+            return ResponseEntity.success("删除成功！");
+        }
+        return ResponseEntity.error("删除失败！");
+    }
 
 
 }
