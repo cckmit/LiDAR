86fd81416016e2240cb9a1e5bca519f04c6cb1d9
shadowLQ <415804410@qq.com>
2022-07-15 14:12:02 +0800

    feat:收款单管理

--- src/main/java/com/lq/lidar/controller/OlRentalReceiptVoucherController.java
+++ src/main/java/com/lq/lidar/controller/OlRentalReceiptVoucherController.java
@@ -1,9 +1,18 @@
 package com.lq.lidar.controller;
 
 
+import com.github.pagehelper.PageInfo;
 import com.lq.lidar.common.core.controller.BaseController;
+import com.lq.lidar.common.core.domain.ResponseEntity;
+import com.lq.lidar.domain.entity.OlRentalContract;
+import com.lq.lidar.domain.entity.OlRentalReceiptVoucher;
+import com.lq.lidar.service.IOlRentalReceiptVoucherService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/system/olRentalReceiptVoucher")
 public class OlRentalReceiptVoucherController extends BaseController {
 
+    @Resource
+    IOlRentalReceiptVoucherService olRentalReceiptVoucherService;
+
+    @GetMapping("/list")
+    public ResponseEntity list(OlRentalReceiptVoucher olRentalReceiptVoucher) {
+        startPage();
+        List<OlRentalReceiptVoucher> list = olRentalReceiptVoucherService.lambdaQuery()
+                .orderByDesc(OlRentalReceiptVoucher::getArriveDate)
+                .orderByDesc(OlRentalReceiptVoucher::getSeqno)
+                .list();
+        return ResponseEntity.success(getDataTable(list));
+    }
 }
 
--- src/main/java/com/lq/lidar/domain/entity/OlRentalReceiptVoucher.java
+++ src/main/java/com/lq/lidar/domain/entity/OlRentalReceiptVoucher.java
@@ -3,9 +3,11 @@ package com.lq.lidar.domain.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -37,7 +39,8 @@ public class OlRentalReceiptVoucher implements Serializable {
      * 收款日期
      */
     @TableField("\"ARRIVE_DATE\"")
-    private LocalDateTime arriveDate;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private LocalDate arriveDate;
 
     /**
      * 实际收款金额
@@ -206,11 +209,11 @@ public class OlRentalReceiptVoucher implements Serializable {
         this.receiptNo = receiptNo;
     }
 
-    public LocalDateTime getArriveDate() {
+    public LocalDate getArriveDate() {
         return arriveDate;
     }
 
-    public void setArriveDate(LocalDateTime arriveDate) {
+    public void setArriveDate(LocalDate arriveDate) {
         this.arriveDate = arriveDate;
     }
 
