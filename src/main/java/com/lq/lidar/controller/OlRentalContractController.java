42dc6b3186431ca12866c4554564b5ea7aaa9a42
shadowLQ <415804410@qq.com>
2022-07-21 18:39:33 +0800

    feat:合同IDLV

--- src/main/java/com/lq/lidar/controller/OlRentalContractController.java
+++ src/main/java/com/lq/lidar/controller/OlRentalContractController.java
@@ -4,11 +4,14 @@ package com.lq.lidar.controller;
 import com.github.pagehelper.PageInfo;
 import com.lq.lidar.common.core.controller.BaseController;
 import com.lq.lidar.common.core.domain.ResponseEntity;
+import com.lq.lidar.common.enums.OlStatusCode;
+import com.lq.lidar.common.utils.DataConvert;
 import com.lq.lidar.domain.entity.OlRentalContract;
 import com.lq.lidar.service.IOlRentalContractService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -36,5 +39,14 @@ public class OlRentalContractController extends BaseController {
         PageInfo<OlRentalContract> list = olRentalContractService.list(olRentalContract);
         return ResponseEntity.success(getDataTable(list));
     }
+
+    @GetMapping("/findContractIdLV")
+    public ResponseEntity findContractIdLV(){
+        List<OlRentalContract> list = olRentalContractService.lambdaQuery().eq(OlRentalContract::getContractStatus, OlStatusCode.CONTRACT_STATUS_EXECUTED.getCode()).list();
+        List lv = DataConvert.ListToLV(list, OlRentalContract::getContractId, OlRentalContract::getContractId);
+        return ResponseEntity.success(lv);
+
+    }
+
 }
 
